01d29305b59a3881e3fd4daee06f5daac625df01
bigman <terrence.chebii@student.moringaschool.com>
2023-03-20 20:38:59 +0300

    done

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-// Code your solution in this file!
-const returnFirstTwoDrivers = function(drivers) {
-    return drivers.slice(0, 2);
-  };
+
   
-  const returnLastTwoDrivers = function(drivers) {
+
+  const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+  }
+
+  const returnLastTwoDrivers = function(drivers){
     return drivers.slice(-2);
-  };
-  
-  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-  
-  const createFareMultiplier = function(multiplier) {
-    return function(fare) {
-      return fare * multiplier;
-    };
-  };
-  
-  const fareDoubler = createFareMultiplier(2);
-  
-  const fareTripler = createFareMultiplier(3);
-  
-  const selectDifferentDrivers = function(drivers, func) {
-    return func(drivers);
-  };
-  
+  }
+
+  let selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers];
+
+  function createFareMultiplier(x){
+    return function multiply(y){
+        return x * y
+    }
+  }
+
+  function fareDoubler(x){
+    return x * 2
+  }
+
+  function fareTripler(x){
+    return x * 3
+  }
+
+  const selectDifferentDrivers = function(drivers , x){
+    if(x = returnFirstTwoDrivers()){
+        return drivers.slice(0 , 2)
+    }else if (x = returnLastTwoDrivers()){
+        return drivers.slice(-2)
+    }
+  }
